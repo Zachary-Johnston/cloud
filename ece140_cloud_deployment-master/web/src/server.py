1fedcab4e6516671ed6b8aceddf3cb79bf5464e3
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-05-04 16:46:48 -0700

    Update server.py

--- ece140_cloud_deployment-master/web/src/server.py
+++ ece140_cloud_deployment-master/web/src/server.py
@@ -28,7 +28,7 @@ def add_new_user(req):
 
   data = {"Username": req.params['Username'], "Password":  req.params['Password']}
   #New_user = requests.post(REST_SERVER + '/new_users', data=data).json()
-  New_user = requests.post('http://64.225.127.211:6001/add_users_db', data=data).json()
+  New_user = requests.post('http://64.225.127.211:6001/new_users', data=data).json()
   return render_to_response('templates/portal.html', {}, request=req)
 
 # This function will become useless
