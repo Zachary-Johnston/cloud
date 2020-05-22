68ea35a0f982c7d389900c21b83d6bf3410cb24c
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-05-22 16:39:29 -0700

    Update init_db.py

--- ece140_cloud_deployment-master/web/src/init_db.py
+++ ece140_cloud_deployment-master/web/src/init_db.py
@@ -42,27 +42,29 @@ values = [
 cursor.executemany(query, values)
 db.commit()
 
-# CREATED NEWS TABLE
-try:
-  cursor.execute("""
-    CREATE TABLE News (
-      id integer  AUTO_INCREMENT PRIMARY KEY,
-      Title  NVARCHAR(max) NOT NULL,
-      Date   NVARCHAR(max) NOT NULL,
-      Update       NVARCHAR(max) NOT NULL
-    );
-  """)
-except:
-  print("Table already exists. Not recreating it.")
 
-# Insert Records into News
-query = "insert into News (Title, Date, Update) values (%s, %s, %s)"
+#NEWS TABLE
+try:
+ cursor.execute("""
+   CREATE TABLE news (
+     id integer  AUTO_INCREMENT PRIMARY KEY,
+     title VARCHAR(50) NOT NULL,
+     release_time VARCHAR(50) NOT NULL,
+     description    VARCHAR(50) NOT NULL
+   );
+ """)
+except:
+ print("Table already exists. Not recreating it.")
+ 
+# Insert Records into cofset
+query = "insert into news (title, release_time, description) values (%s, %s, %s)"
 values = [
-  ('x','y','z'),
-  ('a','b','c'),
+ ('sometitle', 'sometime', 'somedescription'),
 ]
 cursor.executemany(query, values)
 db.commit()
+
+
 
 
 try:
