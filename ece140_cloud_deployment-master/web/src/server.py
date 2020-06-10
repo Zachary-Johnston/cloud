bb70184bcd1bad7bf9ce6aad43de6ed49fa1cf5d
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-06-10 13:22:31 -0700

    Update server.py

--- ece140_cloud_deployment-master/web/src/server.py
+++ ece140_cloud_deployment-master/web/src/server.py
@@ -64,12 +64,8 @@ def get_playlists(req):
   #new_adds = []
   records = {}
   records = Response(body=json.dumps(results))
-  songs = Response(body=json.dumps(songs))
-  date_added = Response(body=json.dumps(date_added))
   records.headers.update({'Access-Control-Allow-Origin': '*',})
-  songs.headers.update({'Access-Control-Allow-Origin': '*',})
-  date_added.headers.update({'Access-Control-Allow-Origin': '*',})
-  return records, songs, artists, date_added
+  return records
 
 
   # Continue paginating through until all results are returned
