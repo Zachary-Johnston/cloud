0b97856cc86e03b6867ea1d33f1b31429e40a136
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-06-10 17:12:21 -0700

    Update server.py

--- ece140_cloud_deployment-master/web/src/server.py
+++ ece140_cloud_deployment-master/web/src/server.py
@@ -44,6 +44,32 @@ def get_playlists(req):
   # Store songs in a tracks array
   tracks = results['items']
   playlist_length = len(tracks)
+  if len(sys.argv) > 3:
+    username = sys.argv[1]
+    playlist_id = sys.argv[2]
+    track_ids = sys.argv[3:]
+  else:
+    print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
+    sys.exit()
+
+
+    
+  scope = 'playlist-modify-private'
+  username = 'zack_johnston'
+  playlist_id = '7xIQR4CjGNAOEXA00Jjfqc'
+  track_ids = []
+  for x in range(0, playlist_length):
+    track_ids.append(results['items'][x]['track']['id'])
+    
+  token = util.prompt_for_user_token(username, scope)
+
+  if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+    print(results)
+  else:
+    print("Can't get token for", username)
   #songs_array = []
   #artists_array = []
   #date_added_array = []
