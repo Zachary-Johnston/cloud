7a289993855d9ef3fe17a6dae88d23a1287ae2fd
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-06-19 17:29:50 -0700

    Update server.py

--- ece140_cloud_deployment-master/web/src/server.py
+++ ece140_cloud_deployment-master/web/src/server.py
@@ -2,8 +2,6 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.renderers import render_to_response
 from pyramid.response import Response
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyClientCredentials
 
 from pyramid.httpexceptions import HTTPFound            # Perform redirects from the backend to other routes
 # NOTE: this is unencrypted but signed session stored in client cookies. It isn't the most secure, but at least it's baked into Pyramid. Shame on Pyramid!
@@ -15,9 +13,7 @@ import json
 import os
 import time
 import paho.mqtt.client as mqtt
-import sys
-import spotipy
-import spotipy.util as util
+
 
 
 db_user = os.environ['MYSQL_USER']
@@ -29,81 +25,6 @@ db_host = os.environ['MYSQL_HOST']
 
 
 
-def spotify(req):
-  return render_to_response('templates/spotify.html', {}, request =req)
-
-def get_playlists(req):
-
-    
-    ## Authentication
-  # Register an app with https://developer.spotify.com/dashboard/ and paste your Client ID and Client Secret on the line below
-  token = util.oauth2.SpotifyClientCredentials(client_id='531bf1de1dc44e71bd4bb4f9c69af7a7', client_secret='0d6921a912534d15b5fed7e75b4f46b2')
-  cache_token = token.get_access_token()
-  spotify = spotipy.Spotify(cache_token) 
- 
-  # Get the first 100 (max) songs in the playlist
-  results = spotify.user_playlist_tracks('spotify:user:zack_johnston', 'spotify:playlist:6dosGTCTRJ5xtA3XM6YTZb', limit=100, offset=0)
-
-  # Store songs in a tracks array
-  tracks = results['items']
-  playlist_length = len(tracks)
-
-  
-  playlist_id = '7xIQR4CjGNAOEXA00Jjfqc'
-  track_ids = []
-  for x in range(0, playlist_length):
-    track_ids.append(results['items'][x]['track']['id'])
-    
-
-  token = util.prompt_for_user_token(
-        username='zack_johnston',
-        scope='playlist-modify-private',
-        client_id='531bf1de1dc44e71bd4bb4f9c69af7a7',
-        client_secret='0d6921a912534d15b5fed7e75b4f46b2',
-        redirect_uri='https://polarcoffee.org/spotify')
-  spotify = spotipy.Spotify(auth=token)
-  spotify.trace = False
-  adds = spotify.user_playlist_add_tracks(username, playlist_id, track_ids)
-  print(adds)
-
-
-
-  #songs_array = []
-  #artists_array = []
-  #date_added_array = []
-  
-  #for x in range(0, playlist_length):
-    #songs_array.append(results['items'][x]['track']['name'])
-    
-  #for i in range(0, playlist_length):
-    #artists_array.append(results['items'][i]['track']['artists'][0]['name'])
-    
-  #for j in range(0, playlist_length):
-    #date_added_array.append(results['items'][j]['added_at'])
-
-  #print(songs)
-  #print(artists)
-  #print(date_added)
-  #print(json.dumps(results))
-  #new_adds = []
-  records = {}
-  records = Response(body=json.dumps(results))
-  records.headers.update({'Access-Control-Allow-Origin': '*',})
-  return records
-
-
-  # Continue paginating through until all results are returned
-  #while results['next']:
-    #results = spotify.next(results)
-    #tracks.extend(results['items'])
-    #for items in (tracks):
-    #new_adds.append(item['name'])
-
-  #print(json.dumps(new_adds))
-
-  #print(json.dumps(track['artists'][0]['name']))
-  #print(json.dumps(track['artists']))
-  #print(json.dumps(track['artists']['name']))
 
 # Route to retrieve the login page
 def login(req):
@@ -743,11 +664,6 @@ if __name__ == '__main__':
   
   ##################################
   
-  config.add_route('spotify', '/spotify')
-  config.add_view(spotify, route_name='spotify', renderer='json')
-  
-  config.add_route('get_playlists', '/get_playlists')
-  config.add_view(get_playlists, route_name='get_playlists', renderer='json')
 
 
 
